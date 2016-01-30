05e93a9b189449bdb5e26e2297de32ed500b1625
Denis Lachance <pernicious.pickle@gmail.com>
2016-01-30 23:38:16 +0000

    added some test scripts for arm motors and (non working) webcam

---
+++ webcamtest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from PIL import Image
+from subprocess import Popen, PIPE
+import select
+import v4l2capture
+
+# Open the video device.
+video = v4l2capture.Video_device("/dev/video0")
+size_x, size_y = video.set_format(640, 480)
+video.create_buffers(4)
+video.queue_all_buffers()
+
+fps, duration = 1, 20
+p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-vcodec', 'mjpeg', 'video.avi'], stdin=PIPE)
+# Start the device. This lights the LED if it's a camera that has one.
+video.start()
+for i in range(fps * duration):
+	print i
+	# Wait for the device to fill the buffer.
+	select.select((video,), (), ())
+	image_data = video.read_and_queue()
+	image = Image.frombytes("RGB", (size_x, size_y), image_data)
+	image.save(p.stdin, 'JPEG')
+video.close()
+p.stdin.close()
+p.wait()
