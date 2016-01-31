d053bfa50d15331636c06b50626f1dd022633833
Denis Lachance <pernicious.pickle@gmail.com>
2016-01-31 16:43:49 +0000

    added time delay to chatbot

--- webcamtest.py
+++ webcamtest.py
@@ -11,16 +11,17 @@ video.create_buffers(4)
 video.queue_all_buffers()
 
 fps, duration = 1, 20
-p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-vcodec', 'mjpeg', 'video.avi'], stdin=PIPE)
+p = Popen(['ffmpeg', '-y', '-i', 'pipe:0', '-vcodec', 'mjpeg', 'video.avi'], stdin=PIPE)
 # Start the device. This lights the LED if it's a camera that has one.
 video.start()
-for i in range(fps * duration):
-	print i
-	# Wait for the device to fill the buffer.
-	select.select((video,), (), ())
-	image_data = video.read_and_queue()
-	image = Image.frombytes("RGB", (size_x, size_y), image_data)
-	image.save(p.stdin, 'JPEG')
+with open('video.mjpg', 'wb') as f:
+	for i in range(fps * duration):
+		print i
+		# Wait for the device to fill the buffer.
+		select.select((video,), (), ())
+		image_data = video.read_and_queue()
+		p.stdin.write(image_data)
+		#image = Image.frombytes("RGB", (size_x, size_y), image_data)
 video.close()
 p.stdin.close()
 p.wait()
