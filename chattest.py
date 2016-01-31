f6cf62fb4cff35333853baa5e9e3742bdc202af8
Denis Lachance <pernicious.pickle@gmail.com>
2016-01-31 02:51:19 +0000

    added pretty much all movement commands and got shadowbanned from twitch chat

--- chattest.py
+++ chattest.py
@@ -1,8 +1,21 @@
 # Set all the variables necessary to connect to Twitch IRC
-import cfg
-import socket
-import time
-import re
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import cfg, socket, time, re, atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+atexit.register(turnOffMotors)
+
+lr_motor = 			mh.getMotor(4)
+shoulder_motor = 	mh.getMotor(3)
+elbow_motor = 		mh.getMotor(1)
+wrist_motor = 		mh.getMotor(2)
 
 def chat(sock, msg):
 	sock.send("PRIVMSG #{} :{}".format(cfg.CHAN, msg))
@@ -32,8 +45,52 @@ while True:
 			username = re.search(r"\w+", r).group(0) # return the entire match
 			message = CHAT_MSG.sub("", r)
 			#print(username + ": " + message)
-			if message == 'l':		#left
+			if message.lower() in ['l', 'left']:		#left
 				print username, "Left"
-			elif message == 'r':
+				lr_motor.setSpeed(255)
+				lr_motor.run(Adafruit_MotorHAT.BACKWARD)
+				time.sleep(0.1)
+				lr_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['r', 'right']:
 				print username, 'Right'
+				lr_motor.setSpeed(255)
+				lr_motor.run(Adafruit_MotorHAT.FORWARD)
+				time.sleep(0.1)
+				lr_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['su', 'shoulder up']:
+				print username, 'Shoulder Up'
+				shoulder_motor.setSpeed(255)
+				shoulder_motor.run(Adafruit_MotorHAT.BACKWARD)
+				time.sleep(0.1)
+				shoulder_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['sd', 'shoulder down']:
+				print username, 'Shoulder Down'
+				shoulder_motor.setSpeed(255)
+				shoulder_motor.run(Adafruit_MotorHAT.FORWARD)
+				time.sleep(0.1)
+				shoulder_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['eu', 'elbow up']:
+				print username, 'Elbow Up'
+				elbow_motor.setSpeed(255)
+				elbow_motor.run(Adafruit_MotorHAT.FORWARD)
+				time.sleep(0.1)
+				elbow_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['ed', 'elbow down']:
+				print username, 'Elbow Down'
+				elbow_motor.setSpeed(255)
+				elbow_motor.run(Adafruit_MotorHAT.BACKWARD)
+				time.sleep(0.1)
+				elbow_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['wu', 'wrist up']:
+				print username, 'Wrist Up'
+				wrist_motor.setSpeed(255)
+				wrist_motor.run(Adafruit_MotorHAT.FORWARD)
+				time.sleep(0.1)
+				wrist_motor.run(Adafruit_MotorHAT.RELEASE)
+			elif message.lower() in ['wd', 'wrist down']:
+				print username, 'Wrist Down'
+				wrist_motor.setSpeed(255)
+				wrist_motor.run(Adafruit_MotorHAT.BACKWARD)
+				time.sleep(0.1)
+				wrist_motor.run(Adafruit_MotorHAT.RELEASE)
 		    #print "Unknown:", r
