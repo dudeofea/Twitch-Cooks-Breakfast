05983adf1d108346a22644e071ab68ced2cb73dd
Denis Lachance <dudeofea@hotmail.com>
2016-01-30 18:21:05 -0700

    added basic chat code detection

--- chattest.py
+++ chattest.py
@@ -22,19 +22,18 @@ s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
 s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
 s.send("JOIN {}\r\n".format(cfg.CHAN).encode("utf-8"))
 
-
 while True:
 	response = s.recv(1024).decode("utf-8")
-	print response
-	# if response == "PING :tmi.twitch.tv\r\n":
-	#     s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-	#     print("Pong")
-	# else:
-	#     username = re.search(r"\w+", line).group(0)  # return the entire match
-	#     message = CHAT_MSG.sub("", line)
-	#     print(username + ": " + message)
-	#     for pattern in cfg.PATT:
-	#         if re.match(pattern, message):
-	#             ban(s, username)
-	#             break
-	#     time.sleep(1 / cfg.RATE)
+	for r in response.split('\r\n'):
+		if r == "PING :tmi.twitch.tv\r\n":
+		    s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+		    print("Pong")
+		elif len(r) > 0:
+			username = re.search(r"\w+", r).group(0) # return the entire match
+			message = CHAT_MSG.sub("", r)
+			#print(username + ": " + message)
+			if message == 'l':		#left
+				print username, "Left"
+			elif message == 'r':
+				print username, 'Right'
+		    #print "Unknown:", r
