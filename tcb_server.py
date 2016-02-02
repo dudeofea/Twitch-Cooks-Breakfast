8754470f8764eb6dfaea993e78459869c0df0167
Denis Lachance <dudeofea@hotmail.com>
2016-02-02 00:33:53 -0700

    added title and what is to be a chatbox

--- tcb_server.py
+++ tcb_server.py
@@ -10,39 +10,64 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 
+PINK1			= (255,	158,174)
+RED1			= (219,	80,	74)
+RED2			= (183,	63,	92)
+DARK			= (34,	29,	35)
+LIGHT			= (252,	247,255)
+
 DEVICE = '/dev/video1'
-SIZE = (0, 0)				#width / height, set at runtime
+SIZE = (1500, 1000)				#width / height, set at runtime
 CAMERA_SIZE = (640, 480)
 FILENAME = 'capture.png'
 
-def camstream():
+class ChatSurface(pygame.Surface):
+	def __init__(self, size_wh, header_font):
+		pygame.Surface.__init__(self, size_wh)
+		self.size_wh = size_wh
+		self.header_font = header_font
+	def draw(self, screen):
+		self.fill(LIGHT)
+		header_label 			= self.header_font.render("Twitch Cooks Breakfast", 1, LIGHT)
+		header_label_shadow 	= self.header_font.render("Twitch Cooks Breakfast", 1, RED1)
+		pygame.draw.rect(self, PINK1, [0, 0, self.size_wh[0], 70])				#header background
+		header_pad = (self.size_wh[0] - header_label.get_width()) / 2
+		self.blit(header_label_shadow, (2 + header_pad, 12))
+		self.blit(header_label, (header_pad, 10))
+		screen.blit(self, (SIZE[0] - self.size_wh[0], 0))
+
+def main():
 	#init game, camera, display
 	pygame.init()
+	leckerli_font = pygame.font.Font("fonts/LeckerliOne-Regular.ttf", 35)
 	pygame.camera.init()
 	infoObject = pygame.display.Info()
-	SIZE = (infoObject.current_w, infoObject.current_h)
-	display = pygame.display.set_mode(SIZE, FULLSCREEN)
+	#SIZE = (infoObject.current_w, infoObject.current_h)
+	screen = pygame.display.set_mode(SIZE, 0)#FULLsurface)
 	camera = pygame.camera.Camera(DEVICE, CAMERA_SIZE)
 	camera.start()
-	#camera screen to draw on display
-	screen = pygame.surface.Surface(CAMERA_SIZE, 0, display)
+	#initialize surfaces
+	camera_surface = pygame.surface.Surface(CAMERA_SIZE, 0, screen)
+	chat_surface = ChatSurface((400, SIZE[1]), leckerli_font)
 	capture = True
 	while capture:
-		screen = camera.get_image(screen)
-		#draw an image on display
-		display.blit(screen, (0,0))
-		#flip the display buffers thus updating the screen
+		#get camera image
+		camera_surface = camera.get_image(camera_surface)
+		#draw the screen elements
+		pygame.draw.rect(screen, DARK, [0, 0, SIZE[0], SIZE[1]])				#background color
+		screen.blit(camera_surface, (0,0))										#camera screen
+		chat_surface.draw(screen)
+		#flip the display buffers thus updating the surface
 		pygame.display.flip()
 		#handle events
 		for event in pygame.event.get():
 			if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
 				capture = False
 			elif event.type == KEYDOWN and event.key == K_s:
-				pygame.image.save(screen, FILENAME)
+				pygame.image.save(surface, FILENAME)
 	#die
 	camera.stop()
 	pygame.quit()
-	return
 
 if __name__ == '__main__':
-	camstream()
+	main()
